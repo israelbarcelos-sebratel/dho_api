0052a139ac9907035cbadf168efba736e8db8d47
Israel da Rosa Barcelos <israel.barcelos@sebratel.com.br>
2026-05-18 17:16:04 -0300

    feat: add TeamController to expose /api/teams endpoint

---
+++ src/main/java/br/com/sebratel/bff/dho/controller/TeamController.java
@@ -0,0 +1,28 @@
+package br.com.sebratel.bff.dho.controller;
+
+import br.com.sebratel.bff.dho.domain.entity.auxiliary.DhoTeam;
+import br.com.sebratel.bff.dho.domain.repository.DhoTeamRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teams")
+@RequiredArgsConstructor
+@Tag(name = "Times", description = "Endpoints para consulta de times")
+public class TeamController {
+
+    private final DhoTeamRepository teamRepository;
+
+    @GetMapping
+    @Operation(summary = "Listar todos os times", description = "Retorna uma lista com todos os times cadastrados no sistema.")
+    public ResponseEntity<List<DhoTeam>> getAllTeams() {
+        return ResponseEntity.ok(teamRepository.findAll());
+    }
+}
